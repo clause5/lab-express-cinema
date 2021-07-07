111c11648e53668d18563ca673eaa80e29b30b14
clause <claussdev5@gmail.com>
2021-07-07 17:15:20 -0400

    all done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
+const { parse } = require('dotenv');
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+/*GET movies page */
+
+router.get('/movies', (req,res, next) => {
+    Movie.find({})
+    .then((allMovies => 
+        res.render('movies' , {movies: allMovies})
+        ))
+        .catch(err =>console.log(err))
+
+    
+});
+
+router.get('/movie/:id', (req,res, next) => {
+    Movie.findById(req.params.id)
+    .then( movie => 
+        res.render('movie', {movie : movie})
+    )
+    .catch(err => console.log(err))
+})
 
 module.exports = router;
